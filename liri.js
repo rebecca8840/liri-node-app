a5eb0bc3d06a56780e54eb6a2142d8d6a8298914
rebecca8840 <31925939+rebecca8840@users.noreply.github.com>
2018-03-02 21:11:04 -0500

    Add files via upload

--- liri.js
+++ liri.js
@@ -4,11 +4,11 @@ var keys = require("./keys.js");
 
 var fs = require("fs");
 
-var require = require("request");
+var request = require("request");
 
-var twitter = require("twitter");
+var Twitter = require("twitter");
 
-var spotifyApi = require("spotify");
+var Spotify = require("node-spotify-api");
 
 var command = process.argv[2];
 
@@ -19,38 +19,35 @@ var client = new Twitter(keys.twitter);
 
 
 	var nodeCommands = function() {
-
 			if (command === "my-tweets") {
 
-				params: {screen_name: strawberymoonm};
+				var params = {screen_name: "strawberymoonm"};
 
-				 client.get("statuses/user_timeline/", params, function(error, data, response){
+				 client.get("statuses/user_timeline", params, function(error, data, response){
 
 				 	if (!error) {
 					 		for (var i = 0; i < data.length && i < 20; i++) {
 					 			console.log(data[i].text + data[i].tweetedAt);
 					 		}
-					 	}
+					}
 
 					});
-				 }
-
-		else if (command === "spotify-this-song") {
-
-				var Song = process.argv;
-
-				var song1 = "";
-
-				for (var i = 3; i < Song.length; i++) {
-
-					Song = song1 + " " + Song[i];
-
-					}
 			}
 
-				if (song1) {
+		else if (command === "spotify-this-song") {
+				var song = process.argv[3] || "All the small things";
 
-					spotify.search({type: "track", query: "song"}, 
+				// var song1 = "";
+
+				// for (var i = 3; i < Song.length; i++) {
+
+				// 	Song = song1 + " " + Song[i];
+				// }
+
+				if (song) {
+								console.log(song);
+
+					spotify.search({type: "track", query: song}, 
 
 						function(err, data) {		
 
@@ -58,24 +55,23 @@ var client = new Twitter(keys.twitter);
 
 						return console.log("Error occurred: " + err);
 
-					}
+						}
 
-					console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name + "\n Song Name: " + Song + "\n Preview Link: " + data.tracks.items[0].preview_url + "\n Album: " + data.tracks.items[0].album.name);
+					console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name + "\n Song Name: " + song + "\n Preview Link: " + data.tracks.items[0].preview_url + "\n Album: " + data.tracks.items[0].album.name);
 					
-					})
+					});
 					
-					;}
-
+				}
+		}
 		else {
 
-			spotify.search({type:"track", query: "The Sign"}), function (err, data) {
-				if (err){
+			spotify.search({type:"track", query: "The Sign"}, function (err2, data) {
+				if (err2){
 					return console.log("Error: " + error);
 					}
 				console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name + "\n Song Name: 'The Sign'" + "Preview Link: " + data.tracks.items[0].preview_url + "\n Album: " + data.tracks.items[0].album.name);
-		};
+		});
 	}
-}
 
 		if (command === "movie-this") {
 
@@ -93,9 +89,9 @@ var client = new Twitter(keys.twitter);
 
 		if (movie) {
 
-				request(queryUrl, function(error, response, body) {
+				request(queryUrl, function(error3, response, body) {
 
-				if (!error) {
+				if (!error3) {
 
 				console.log("Movie Title: " + JSON.parse(body).Title + "\n Year: " + JSON.parse(body).Year + "\n IMDB Rating: " + JSON.parse(body).imdbRating + "\n Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n Country: " + JSON.parse(body).Country + "\n Lanugage: " + JSON.parse(body).Plot + "\n Actors: " + JSON.parse(body).Actors);
 					
@@ -106,32 +102,33 @@ var client = new Twitter(keys.twitter);
 
 		else {
 
-			if (!error) {
+		// 	if (!error) {
 
-				var queryUrl = "http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy";
+		// 		var queryUrl = "http://www.omdbapi.com/?t=mr.nobody&y=&plot=short&apikey=trilogy";
 
-				request(queryUrl, function(error, response, body) {
-					console.log("Movie Title: " + JSON.parse(body).Title + "\n Year: " + JSON.parse(body).Year + "\n IMDB Rating: " + JSON.parse(body).imdbRating + "\n Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n Country: " + JSON.parse(body).Country + "\n Lanugage: " + JSON.parse(body).Plot + "\n Actors: " + JSON.parse(body).Actors);    
-		  });
-		};
+		// 		request(queryUrl, function(error, response, body) {
+		// 			console.log("Movie Title: " + JSON.parse(body).Title + "\n Year: " + JSON.parse(body).Year + "\n IMDB Rating: " + JSON.parse(body).imdbRating + "\n Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\n Country: " + JSON.parse(body).Country + "\n Lanugage: " + JSON.parse(body).Plot + "\n Actors: " + JSON.parse(body).Actors);    
+		//   		});
+		
 
-
+		// }
+	}
 		
 		if (command === 'do-what-it-says') {
 
 
-	fs.readFile("random.txt", "utf8", function(error, data) {
+	fs.readFile("random.txt", "utf8", function(error4, data) {
 
 
-		if (error) {
+		if (error4) {
 			return console.log(error);
 		}
 
 		var dataArr = data.split(",");
 
-		spotify.search({type: 'track', query: dataArr[1]}, function(err, data) {
+		spotify.search({type: 'track', query: dataArr[1]}, function(err2, data) {
 
-			if(err) {
+			if(err2) {
 
 				return console.log("Error Occurred: " + err);
 			}
@@ -140,4 +137,7 @@ var client = new Twitter(keys.twitter);
 		});
 
 	});
-};
+}
+}
+
+nodeCommands();
